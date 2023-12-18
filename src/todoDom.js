280dc26b7cc6eef5e990d359225b56f4e0f8907c
ragul2k23 <rockyragulsiva2005@gmail.com>
2023-12-18 22:44:58 +0530

    Test publishing

--- src/todoDom.js
+++ src/todoDom.js
@@ -11,8 +11,8 @@ function makeTaskContainer(obj,pushIndex){
         const task = document.createElement('div');task.classList.add('task');
         task.innerHTML= ` <div  class="toDoTask ${taskPriority} s"><input type="checkbox" name="completeTask" id="completeTask">
         <div>${taskName}</div><div>${taskDue}</div> <div>${taskDescription}</div>
-        <div class="img"><img data-name="${taskName}${taskPriority}${taskDescription}" class=edit src="./images/pen.f143f2542420df9040ba2f60576c01b4.svg" alt="pen">
-        <img tooltip=delete  class=delete data-name="${taskName}${taskPriority}${taskDescription}" src="./images/icons8-trash-1-dark.912351d015e21b5e38469d33950ebd1b.svg" alt="bin"> </div>              </div>
+        <div class="img"><img data-name="${taskName}${taskPriority}${taskDescription}" class=edit src="./dist/images/pen.f143f2542420df9040ba2f60576c01b4.svg" alt="pen">
+        <img tooltip=delete  class=delete data-name="${taskName}${taskPriority}${taskDescription}" src="./dist/images/icons8-trash-1-dark.912351d015e21b5e38469d33950ebd1b.svg" alt="bin"> </div>              </div>
         </div>` 
         task.setAttribute('data-todoname',`${taskName}${taskPriority}${taskDescription}`);   
         pushTask(domTasks,task,pushIndex)
